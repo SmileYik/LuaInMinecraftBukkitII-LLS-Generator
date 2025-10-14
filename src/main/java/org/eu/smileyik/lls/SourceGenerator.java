9f3167d67a56548409ef592cb117632401773085
SmileYik <miskyle@outlook.com>
2025-10-15 00:58:56 +0800

    update SourceGenerator.java

--- src/main/java/org/eu/smileyik/lls/SourceGenerator.java
+++ src/main/java/org/eu/smileyik/lls/SourceGenerator.java
@@ -14,6 +14,7 @@ import java.nio.file.StandardOpenOption;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Locale;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
@@ -22,27 +23,36 @@ import java.util.zip.ZipFile;
 public class SourceGenerator {
     private static final JavaParser PARSER = new JavaParser(new ParserConfiguration().setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_21));
 
-    public static interface Splitter extends Function<ClassMeta, String> {}
+    public static interface Splitter extends BiFunction<String, ClassMeta, String> {}
 
     public static void generate(@NotNull Path sourcePath,
                                 @NotNull Path outputDir,
                                 @NotNull Splitter splitter) throws IOException {
-        try (Stream<Path> list = Files.list(sourcePath)) {
+        generate(sourcePath, sourcePath, outputDir, splitter);
+    }
+
+    protected static void generate(@NotNull Path sourcePath,
+                                   @NotNull Path path,
+                                   @NotNull Path outputDir,
+                                   @NotNull Splitter splitter) throws IOException {
+        String sourcePathStr = sourcePath.toAbsolutePath().normalize().toString();
+        try (Stream<Path> list = Files.list(path)) {
             list.forEach(it -> {
                 if (Files.isDirectory(it)) {
                     try {
-                        generate(it, outputDir, splitter);
+                        generate(sourcePath, it, outputDir, splitter);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 } else if (it.getFileName().toString().toLowerCase(Locale.ENGLISH).endsWith(".java")) {
                     try {
                         PARSER.parse(it).ifSuccessful(cu -> {
+                            String reactivePath = it.toAbsolutePath().normalize().toString().substring(sourcePathStr.length());
                             SourceVisitor sourceVisitor = new SourceVisitor();
                             cu.accept(sourceVisitor, null);
                             Collection<ClassMeta> classMetas = sourceVisitor.getClassMetas();
                             for (ClassMeta classMeta : classMetas) {
-                                String result = splitter.apply(classMeta);
+                                String result = splitter.apply(reactivePath, classMeta);
                                 if (result != null) {
                                     Path out = outputDir.resolve(result);
                                     try {
@@ -80,7 +90,7 @@ public class SourceGenerator {
                         cu.accept(sourceVisitor, null);
                         Collection<ClassMeta> classMetas = sourceVisitor.getClassMetas();
                         for (ClassMeta classMeta : classMetas) {
-                            String result = splitter.apply(classMeta);
+                            String result = splitter.apply(entry.getName().startsWith("/") ? entry.getName() : "/" + entry.getName(), classMeta);
                             if (result != null) {
                                 Path out = outputDir.resolve(result);
                                 try {
